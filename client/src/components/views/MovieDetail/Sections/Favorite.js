643ca8b7c22ecee5422a78f9fee23d1a8276747a
silverz <liisa9601@naver.com>
2021-09-01 19:22:35 +0900

    favorite 버튼 구현

--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,5 +1,6 @@
 import Axios from 'axios'
-import React , { useEffect } from 'react'
+import React , { useEffect,useState } from 'react'
+import { Button } from 'antd';
 
 function Favorite(props) {
 
@@ -9,31 +10,74 @@ function Favorite(props) {
     const moviePost = props.movieInfo.backdrop_path
     const movieRunTime = props.movieInfo.runtime 
 
+    const [FavoriteNumber, setFavoriteNumber] = useState(0)
+    const [Favorited, setFavorited] = useState(false)
+
+    let variables={
+        userFrom: userFrom,
+        movieId: movieId,
+        movieTitle: movieTitle,
+        moviePost: moviePost,
+        movieRunTime: movieRunTime
+    }
+
     useEffect(() => {
         
-        let variables={
-            userFrom: userFrom,
-            movieId: movieId,
-            movieTitle: movieTitle,
-            moviePost: moviePost,
-            movieRunTime: movieRunTime
-        }
+        
 
         Axios.post('/api/favorite/favoriteNumber',variables)
         .then(response => {
             if(response.data.success){
-
+                setFavoriteNumber(response.data.favoriteNumber)
             }else{
                 alert('숫자 정보를 가져오는데 실패하였습니다.')
+            }
+        })
+
+        Axios.post('/api/favorite/favorited',variables)
+        .then(response => {
+            if(response.data.success){
+                setFavorited(response.data.favorited)
+            }else{
+                alert('정보를 가져오는데 실패하였습니다.')
             }
         })
 
     }, [])
 
 
+    const onClickFavorite = () => {
+
+        if (Favorited) {
+            Axios.post('/api/favorite/removeFromFavorite', variables)
+                .then(response => {
+                    if (response.data.success) {
+                        setFavoriteNumber(FavoriteNumber - 1)
+                        setFavorited(!Favorited)
+                    } else {
+                        alert('Favorite 리스트에서 지우는 것을 실패했습니다.')
+                    }
+                })
+
+
+        } else {
+            Axios.post('/api/favorite/addToFavorite', variables)
+                .then(response => {
+                    if (response.data.success) {
+                        setFavoriteNumber(FavoriteNumber + 1)
+                        setFavorited(!Favorited)
+
+                    } else {
+                        alert('Favorite 리스트에서 추가하는 것을 실패했습니다.')
+                    }
+                })
+        }
+
+    }
+
     return (
         <div>
-            <button>Favorite</button>
+            <Button onClick={onClickFavorite}>{Favorited ? " Not Favorite" : "Add to Favorite "}  {FavoriteNumber}  </Button>
         </div>
     )
 }
